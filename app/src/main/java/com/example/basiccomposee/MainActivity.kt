ccdc917def22061f0358dc78ccb3c1717418a38f
elwinspranata <elwinpranata02@gmail.com>
2024-10-19 11:17:52 +0700

    Kode tersebut membuat kolom yang isinya di tengah layar penuh, baik secara horizontal maupun vertikal.

--- app/src/main/java/com/example/basiccomposee/MainActivity.kt
+++ app/src/main/java/com/example/basiccomposee/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basiccomposee.ui.theme.BasicComposeeTheme
@@ -30,17 +33,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingPreview(elwin: Modifier = Modifier){
+    Column(
+        verticalArrangement =  Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+
+
     BasicComposeeTheme {
         Greeting("Android")
     }
